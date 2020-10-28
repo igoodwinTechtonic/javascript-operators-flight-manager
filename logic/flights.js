e8f0bf21798cbe63963ef128c2baebf304dea877
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-10-28 17:32:35 -0500

    Initial commit

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,18 @@
 function Flights() {
+    function calculateNumberOfFlights(numPassengers, flightCapacity) {
+        var numFlights = 0;
 
+        // If numPassengers is a multiple of flightCapacity
+        if (numPassengers % flightCapacity === 0) {
+            /* Need a number of flights equal to:
+               numPassengers / flightCapacity */
+            numFlights = numPassengers / flightCapacity;
+        } else {
+            numFlights = Math.floor(numPassengers / numFlights) + 1;
+        }
+        return numFlights;
+    }
+    return {calculateNumberOfFlights};
 }
+module.exports = Flights();
 
